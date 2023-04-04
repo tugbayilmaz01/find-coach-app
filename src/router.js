8d649ebb93c1392757c7c901c90c9f2533853d06
tugbayilmaz01 <97852394+tugbayilmaz01@users.noreply.github.com>
2023-04-04 15:18:21 +0300

    added route, page and components

--- src/router.js
+++ src/router.js
@@ -1,17 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachList from './pages/coaches/CoachList.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import NotFound from './pages/NotFound.vue';
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/coaches' },
-    { path: '/coaches', component: null },
+    { path: '/coaches', component: CoachList },
     {
       path: '/coaches:id',
-      component: null,
-      children: [{ path: 'contact', component: null }],
+      component: CoachDetail,
+      children: [{ path: 'contact', component: ContactCoach }],
     },
-    { path: '/register', component: null },
-    { path: '/requests', component: null },
-    { path: '/:notFound(.*)', component: null },
+    { path: '/register', component: CoachRegistration },
+    { path: '/requests', component: RequestsReceived },
+    { path: '/:notFound(.*)', component: NotFound },
   ],
 });
 
