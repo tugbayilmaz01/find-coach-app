9eff43752b810f00d4fe0cbf5cb63ff1bffeb98e
tugbayilmaz01 <97852394+tugbayilmaz01@users.noreply.github.com>
2023-04-04 15:39:53 +0300

    added main layout and styling

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import CoachDetail from './pages/coaches/CoachDetail.vue';
-import CoachList from './pages/coaches/CoachList.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
 import CoachRegistration from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
@@ -10,7 +10,7 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/coaches' },
-    { path: '/coaches', component: CoachList },
+    { path: '/coaches', component: CoachesList },
     {
       path: '/coaches:id',
       component: CoachDetail,
