a19b5d9a33404d22aa459cc160a3022ece12fcf4
tugbayilmaz01 <97852394+tugbayilmaz01@users.noreply.github.com>
2023-04-04 15:53:03 +0300

    wired up pages

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,3 @@
+<template>
+  <p>aa</p>
+</template>
--- src/router.js
+++ src/router.js
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
+
 import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import CoachRegistation from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
@@ -12,11 +13,13 @@ const router = createRouter({
     { path: '/', redirect: '/coaches' },
     { path: '/coaches', component: CoachesList },
     {
-      path: '/coaches:id',
+      path: '/coaches/:id',
       component: CoachDetail,
-      children: [{ path: 'contact', component: ContactCoach }],
+      children: [
+        { path: 'contact', component: ContactCoach }, // /coaches/c1/contact
+      ],
     },
-    { path: '/register', component: CoachRegistration },
+    { path: '/register', component: CoachRegistation },
     { path: '/requests', component: RequestsReceived },
     { path: '/:notFound(.*)', component: NotFound },
   ],
