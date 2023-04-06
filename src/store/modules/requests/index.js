0a0902f23964b2db3b5b0968b13ae46012789768
tugbayilmaz01 <97852394+tugbayilmaz01@users.noreply.github.com>
2023-04-06 11:35:46 +0300

    storing requests with vuex

---
+++ src/store/modules/requests/index.js
@@ -0,0 +1,12 @@
+import mutations from './mutations.js';
+import actions from './actions.js';
+export default {
+  namespaced: true,
+  state() {
+    return {
+      requests: [],
+    };
+  },
+  mutations,
+  actions,
+};
