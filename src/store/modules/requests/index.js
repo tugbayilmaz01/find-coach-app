cb509a446f6fd8e037f3fa6924e17b7e0d25ba86
tugbayilmaz01 <97852394+tugbayilmaz01@users.noreply.github.com>
2023-04-06 11:58:18 +0300

    outputting incoming requests(messages)

--- src/store/modules/requests/index.js
+++ src/store/modules/requests/index.js
@@ -1,5 +1,7 @@
 import mutations from './mutations.js';
 import actions from './actions.js';
+import getters from './getters.js';
+
 export default {
   namespaced: true,
   state() {
@@ -9,4 +11,5 @@ export default {
   },
   mutations,
   actions,
+  getters,
 };
